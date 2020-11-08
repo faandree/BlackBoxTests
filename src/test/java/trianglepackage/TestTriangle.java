07c625021438f6fcb1b781d9b22f1e97239656f0
João Cunha <skidrow.jc@gmail.com>
2020-11-08 18:25:37 +0000

    updates

--- src/test/java/trianglepackage/TestTriangle.java
+++ src/test/java/trianglepackage/TestTriangle.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestTriangle {
+	int max = Integer.MAX_VALUE;
 
 	/**
 	 * Test if is impossible
@@ -105,51 +106,107 @@ public class TestTriangle {
 		Assertions.assertNotEquals("scalene", t.classify(), "Should not be scalene");
 	}
 
-	/**
-	 * Test blow minimum mening > 0
-	 */
 	@Test
 	public void testClassify_BVA_1() {
-		Triangle t = new Triangle(-1,-1,-1);
+		Triangle t = new Triangle(1, 1, max + 1);
 		Assertions.assertTrue(t.isImpossible());
-		Assertions.assertEquals("impossible", t.classify(), "");
+		Assertions.assertEquals("impossible", t.classify(), "Should be impossible");
 	}
 
-	/**
-	 * Test blow maximum value
-	 */
 	@Test
 	public void testClassify_BVA_2() {
-		int i = Integer.MAX_VALUE;
-		Triangle t = new Triangle(i, i, i);
-		Assertions.assertFalse(t.isImpossible());
-		Assertions.assertEquals("equilateral", t.classify(), "");
+		Triangle t = new Triangle(1, max + 1, 1);
+		Assertions.assertTrue(t.isImpossible());
+		Assertions.assertEquals("impossible", t.classify(), "Sould be impossible");
 	}
 
-	/**
-	 * Test blow maximum value + 1
-	 */
 	@Test
 	public void testClassify_BVA_3() {
-		int i = Integer.MAX_VALUE + 1;
-		Triangle t = new Triangle(i, i, i);
+		Triangle t = new Triangle(1, max + 1, max + 1);
 		Assertions.assertTrue(t.isImpossible());
 		Assertions.assertEquals("impossible", t.classify(), "");
 	}
 
-	/**
-	 * Test blow with in negative value
-	 */
 	@Test
 	public void testClassify_BVA_4() {
+		Triangle t = new Triangle(max + 1, 1, 1);
+		Assertions.assertTrue(t.isImpossible());
+		Assertions.assertEquals("impossible", t.classify(), "");
+	}
+
+	@Test
+	public void testClassify_BVA_5() {
+		Triangle t = new Triangle(max + 1, 1, max + 1);
+		Assertions.assertTrue(t.isImpossible());
+		Assertions.assertEquals("impossible", t.classify(), "");
+	}
+
+	@Test
+	public void testClassify_BVA_6() {
+		Triangle t = new Triangle(max + 1, max + 1, max + 1);
+		Assertions.assertTrue(t.isImpossible());
+		Assertions.assertEquals("impossible", t.classify(), "");
+	}
+
+	@Test
+	public void testClassify_BVA_7() {
+		Triangle t = new Triangle(1, 1, -1);
+		Assertions.assertTrue(t.isImpossible());
+		Assertions.assertEquals("impossible", t.classify(), "");
+	}
+
+	@Test
+	public void testClassify_BVA_8() {
+		Triangle t = new Triangle(1, -1, 1);
+		Assertions.assertTrue(t.isImpossible());
+		Assertions.assertEquals("impossible", t.classify(), "");
+	}
+
+	@Test
+	public void testClassify_BVA_9() {
+		Triangle t = new Triangle(1, -1, -1);
+		Assertions.assertTrue(t.isImpossible());
+		Assertions.assertEquals("impossible", t.classify(), "");
+	}
+
+	@Test
+	public void testClassify_BVA_10() {
 		Triangle t = new Triangle(-1, 1, 1);
+		Assertions.assertTrue(t.isImpossible());
+		Assertions.assertEquals("impossible", t.classify(), "");
+	}
+
+	@Test
+	public void testClassify_BVA_11() {
+		Triangle t = new Triangle(-1, 1, -1);
+		Assertions.assertTrue(t.isImpossible());
+		Assertions.assertEquals("impossible", t.classify(), "");
+	}
+
+	@Test
+	public void testClassify_BVA_12() {
+		Triangle t = new Triangle(-1, 1, -1);
+		Assertions.assertTrue(t.isImpossible());
+		Assertions.assertEquals("impossible", t.classify(), "");
+	}
+
+	@Test
+	public void testClassify_BVA_13() {
+		Triangle t = new Triangle(-1, -1, 1);
+		Assertions.assertTrue(t.isImpossible());
+		Assertions.assertEquals("impossible", t.classify(), "");
+	}
+
+	@Test
+	public void testClassify_BVA_14() {
+		Triangle t = new Triangle(-1, -1, -1);
 		Assertions.assertTrue(t.isImpossible());
 		Assertions.assertEquals("impossible", t.classify(), "");
 	}
 
 	/*
 	@Test
-	public void testClassify_10() {
+	public void testClassify_BVA_15() {
 		Triangle t = new Triangle(2, 3, null);
 		Assertions.assertEquals("isosceles", t.classify(), "Classify Isosceles Triangle");
 	}
